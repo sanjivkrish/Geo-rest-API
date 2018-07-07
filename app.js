f4732e20e889464f70d5f83ecfc57fed27341129
Sanjiv Krish <sanjiv436.k@gmail.com>
2018-07-07 20:37:15 +0200

    App: Include dynamic port for Heroku support
    Signed-off-by: Sanjiv Krish <sanjiv436.k@gmail.com>

--- app.js
+++ app.js
@@ -6,4 +6,4 @@ const app = new Koa();
 app.use(router());
 
 // Listen to the port '3000'
-app.listen(3000, () => console.log('server started at port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('server started at port 3000'));
