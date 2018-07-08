8835c9d2e5f4ae282450bfeb0cff250fdfc9cc93
Sanjiv Krish <sanjiv436.k@gmail.com>
2018-07-08 17:43:29 +0200

    app: Connect to MongoDB before listening to a port
    Signed-off-by: Sanjiv Krish <sanjiv436.k@gmail.com>

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const router = require('./routes');
 const app = new Koa();
+let initDB = require('./dbs/index');
 
 // Body parser to access data from request
 app.use(bodyParser());
@@ -9,5 +10,9 @@ app.use(bodyParser());
 // Use routes from the 'routes.js'
 app.use(router());
 
-// Listen to the port '3000'
-app.listen(process.env.PORT || 3000, () => console.log('server started at port 3000'));
+initDB().then ((db) => {
+    global.db = db.collection('restaurantList');
+
+    // Listen to the port '3000'
+    app.listen(process.env.PORT || 3000, () => console.log('server started at port 3000'));
+});
