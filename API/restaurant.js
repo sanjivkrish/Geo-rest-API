cd7e42847d43b670643bee4eb3fff5d09a27bc8f
Sanjiv Krish <sanjiv436.k@gmail.com>
2018-07-08 10:28:39 +0200

    API/restaurant: Return restaurant info on GET call
    Signed-off-by: Sanjiv Krish <sanjiv436.k@gmail.com>

--- API/restaurant.js
+++ API/restaurant.js
@@ -1,9 +1,13 @@
 const Router = require ('koa-router');
 const router = new Router({ prefix: '/restaurant' });
 
+// Store restaurant details / Replaced by DB later
+let restaurantList = [];
+
 // get available restaurant details
 router.get('/', async (ctx, next) => {
-  ctx.body = 'Restaurant details'
+  ctx.status = 200;
+  ctx.body = restaurantList;
 })
 
 module.exports = router;
